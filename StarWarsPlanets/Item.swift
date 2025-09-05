9b8fde03c9e1639a4f2ad1a746056b3d827b0c30
dinitha.gamage <dinith.gamage@ifs.com>
2025-09-05 08:25:12 +0530

    Initial Commit

---
+++ StarWarsPlanets/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  StarWarsPlanets
+//
+//  Created by Dinitha Gamage on 2025-09-05.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
